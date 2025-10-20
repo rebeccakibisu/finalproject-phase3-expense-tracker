37a1aacd1a756425e4b3642c4655075fce563453
rebeccakibisu <coursecpd@gmail.com>
2025-10-20 22:21:13 +0300

    updating main with narations

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+# main.py
+# This is the main entry point of the Expense Tracker & Budget Monitor application.
+# It initializes the database tables (if they don’t exist)
+# and launches the Command-Line Interface (CLI) for user interaction.
+
 from lib.database import create_tables
 from lib.cli import main_menu
 
 if __name__ == "__main__":
-    create_tables()
-    main_menu()
+    create_tables()   # Create database tables if not yet created
+    main_menu()       # Start the interactive CLI
